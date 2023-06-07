2c2388073d99154187f51f90c5f8528331b0fd96
Anna Ibarra <annibrra@gmail.com>
2023-06-06 22:14:32 -0500

    Create restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Clear database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants'
+
+my_array = %w[chinese italian japanese french belgian]
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    adress: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: my_array.sample
+  )
+  puts "Restaurant with id: #{restaurant.id} has been created"
+end
+
+puts 'Finished'
